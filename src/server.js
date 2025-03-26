bc107b40c651e235622d15fb80582825a213b25d
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-03-26 17:58:22 -0400

    feat: insert and test GET method

--- src/server.js
+++ src/server.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+        Mensagem ID: ${id}.
+        Para o usuário: ${user}
+        `);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`
+        Page: ${page}. 
+        Limite: ${limit}
+        `);
+});
+
 const port = 3333;
 
 app.listen(port, () => console.log(`Server is running on Port ${port}`));
