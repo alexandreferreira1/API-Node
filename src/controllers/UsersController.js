714e749b9bd1318b9910f4d649d354e4d3d044cc
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-04-02 19:35:25 -0400

    feat: add lib and function to handle errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError.js')
+
 class UsersController {
     create(request, response) {
         const { name, email, password } = request.body
+
+        if (!name) {
+            throw new AppError('O nome é obrigatório!')
+        }
         
         response.json({name, email, password})
     }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
+require('express-async-errors')
+const AppError = require('./utils/AppError.js')
 const express = require("express");
 const app = express();
 const routes = require("./routes/index.js");
 
-const port = 3333;
 
 app.use(express.json());
 
 app.use(routes)
 
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message,
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+    })
+})
+
+const port = 3333;
 app.listen(port, () => console.log(`Server is running on Port ${port}`));
