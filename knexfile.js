091ce65166cf2b0ae2741dce78f52d2d985e5953
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-06-28 22:27:58 -0400

    FEAT: add initial config knex.js

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    }, 
+    useNullAsDefault: true
+  },
+};
