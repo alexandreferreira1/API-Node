48f18163ed5362fc4c46f8a2cbff96f63abd7b6a
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-07-18 17:31:00 -0400

    feat: create migrations notes, tags and links

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
 
   development: {
@@ -5,6 +7,14 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     }, 
+    pool: {
+      afterCreate: (connection, callback) => {
+        connection.run('PRAGMA foreign_keys = ON', callback)
+      }
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault: true
   },
 };
