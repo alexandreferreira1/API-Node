d3da4902dd8f2a79ea81acb4a7ef069accc9dd42
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-07-19 23:50:38 -0400

    feat: add route to create note

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const { Router } = require("express");
 const routes = Router();
 
 const usersRouter = require("./users.routes.js");
+const notesRouter = require("./notes.routes.js");
 
 routes.use("/users", usersRouter);
+routes.use("/notes", notesRouter);
 
 module.exports = routes;
